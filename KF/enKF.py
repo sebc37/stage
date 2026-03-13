4fb5f940fe93577a8de80e12b100ddc2347d50b8
sebc37 <sebastien.calme98@gmail.com>
2026-03-13 17:37:01 +0100

    update enKF

--- KF/enKF.py
+++ KF/enKF.py
@@ -91,7 +91,7 @@ lmb = 2.0
 Data_filtered, Data_train, mean, Var_mode, Std_mode, perc, Y = filter_mode(Data_shell,2*k_min_collocation,2*k_max_collocation,0,0.001,123456)
 Data_shell = reduced_center(Data_shell,mean=mean,std=Std_mode)
 
-K = [k0*lmb**i for i in range(22)]
+K = np.array([k0*lmb**i for i in range(22)],dtype=np.float32)
 
 y_obs = Y.T
 
@@ -106,7 +106,7 @@ y_obs = Y.T
 
 
 ### parameters
-n     = 22 # state size  on veut estimer les Un de 1 à 10 avec Re et Im donc 20 variables d'état
+n     = 44 # state size  on veut estimer les Un de 1 à 10 avec Re et Im donc 20 variables d'état
 p     = 12 # On observe Un n=5,6,7,8,9,10 avec Re et Im donc 12 variables d'observations 
 nb    = Npts # number of times
 time  = np.array(range(nb)) # time vector
@@ -129,9 +129,9 @@ R      = var_R*np.eye(p,p)
 # x = c_[x1, x2, x1_dot, x2_dot].T # true state
 # y = c_[x1, x2].T + randn(p,nb) # noisy observations
 
-x_past = np.random.normal(0,1,size=((n,2))) # state at time t-1 and t-2 for the model m
+
 ### nonlinear and linear operators of the state-space model
-def NL(x_past_real,x_past_imag,t):
+def NL(x_past_real,x_past_imag):
     
     eps = 0.5
     lmb = 2.0
@@ -175,21 +175,21 @@ def NL(x_past_real,x_past_imag,t):
 
 def m(x_past):
 
-    dT = 1.0e-5
+    dT = 1.0e-3
     eps = 0.5
     lmb = 2.0
     nu = 1.0e-7
 
-    x_past_real = x_past[0::2,0] # Reels
-    x_past_imag = x_past[1::2,0] # Imaginaires
+    x_past_real = np.copy(x_past[0::2,0]) # Reels
+    x_past_imag = np.copy(x_past[1::2,0]) # Imaginaires
 
-    x_future_real = x_past[0::2,1] # Un Reels
-    x_future_imag = x_past[1::2,1] # Un Imagin
+    x_future_real = np.copy(x_past[0::2,1]) # Un Reels
+    x_future_imag = np.copy(x_past[1::2,1]) # Un Imagin
 
     n = np.shape(x_past_real)[0]
 
-    NL_re_pp, NL_im_pp = NL(x_past_real,x_past_imag,t=0)
-    NL_re_p, NL_im_p = NL(x_future_real,x_future_imag,t=0)
+    NL_re_pp, NL_im_pp = NL(x_past_real,x_past_imag)
+    NL_re_p, NL_im_p = NL(x_future_real,x_future_imag)
     
     x_future = np.zeros((2*n,2))
     x_future[:,0] = x_past[:,1] # x(t-1) => x(t)
@@ -201,53 +201,69 @@ def m(x_past):
             x_future_r[i] = np.exp(-nu*(K[i]**2)*dT)*(x_future_real[i] + dT*((3/2)*NL_re_p[i] - (1/2)*NL_re_pp[i]))
 
         else:
-            x_future_i[i] = np.exp(-nu*(K[i]**2)*dT)*(x_future_imag[i] + dT*((3/2)*NL_im_p[i] - (1/2)*NL_im_pp[i]))
-            x_future_r[i] = np.exp(-nu*(K[i]**2)*dT)*(x_future_real[i] + dT*((3/2)*NL_re_p[i] - (1/2)*NL_re_pp[i]))  # x1(t+1) = x1(t) + x1_dot(t)
+            x_future_i[i] = np.exp(-nu*(K[i]**2)*dT)*(x_future_imag[i] + dT*((3/2)*NL_im_p[i] - (1/2)*NL_im_pp[i])) + 0.005*dT*np.random.normal(0,1) # on ajoute du bruit pour le mode 4
+            x_future_r[i] = np.exp(-nu*(K[i]**2)*dT)*(x_future_real[i] + dT*((3/2)*NL_re_p[i] - (1/2)*NL_re_pp[i])) + 0.005*dT*np.random.normal(0,1) # x1(t+1) = x1(t) + x1_dot(t)
         
-        x_future[2*i,1] = x_future_real[i]
-        x_future[2*i+1,1] = x_future_imag[i]
+        x_future[2*i,1] = x_future_r[i]
+        x_future[2*i+1,1] = x_future_i[i]
     
     return x_future
+
+nu = 1.0e-7
+x_past = np.zeros((n,2))
+x_past[0::2,0] = K**(-1./3) #[0:int(n/2)]
+x_past[1::2,0] = 1e-4 
+x_p_r,x_p_i = NL(x_past[0::2,0],x_past[1::2,0])
+x_past[0::2,1] = np.exp(-nu*K**2*1.0e-5)*(x_past[0::2,0] + 1.0e-5*x_p_r ) #np.random.normal(0,1.e-4,size=((n,2))) # state at time t-1 and t-2 for the model m
+x_past[1::2,1] = np.exp(-nu*K**2*1.0e-5)*(x_past[1::2,0] + 1.0e-5*x_p_i ) #np.random.normal(0,1.e-4,size=((n,2))) # state at time t-1 and t-2 for the model m
 
 series = np.zeros((n,100))
 series[:,0] = x_past[:,0]
 series[:,1] = x_past[:,1]
 
-for i in range(2,100):
-    update = m(x_past)
-    series[:,i] = update[:,1]
-    x_past = update
+# for i in range(2,100):
+#     update = m(series[:,i-2:i])
+#     series[:,i] = update[:,1]
+#     #x_past = update
 
-H = array([[1,0,0,0], [0,1,0,0]])
+# plt.figure()
+# for i in range(10):
+#     plt.plot(series[2*i,:])
+# plt.show()
+
+H = np.eye(44,44) #array([[1,0,0,0], [0,1,0,0]])
+H = H[2*k_min_collocation:2*k_max_collocation,:] # on observe que les modes de 5 à 10 avec Re et Im donc 12 variables d'observations
 
 ### Ensemble Kalman initialization
 Ne = 100                   # number of ensembles
-x_f_enkf = zeros((n,nb))   # forecast state
-P_f_enkf = zeros((n,n,nb)) # forecast error covariance matrix
-x_a_enkf = zeros((n,nb))   # analysed state
-P_a_enkf = zeros((n,n,nb)) # analysed error covariance matrix
+x_f_enkf = np.zeros((n,nb))   # forecast state
+P_f_enkf = np.zeros((n,n,nb)) # forecast error covariance matrix
+x_a_enkf = np.zeros((n,nb))   # analysed state
+P_a_enkf = np.zeros((n,n,nb)) # analysed error covariance matrix
 
 ### Ensemble Kalman filter
-x_a_enkf_tmp = zeros((n,Ne)) 
-x_f_enkf_tmp = zeros((n,Ne))
-y_f_enkf_tmp = zeros((p,Ne))
+x_a_enkf_tmp = np.zeros((n,Ne)) 
+x_f_enkf_tmp = np.zeros((n,Ne))
+y_f_enkf_tmp = np.zeros((p,Ne))
 # initial step
 for i in range(Ne):
-    x_a_enkf_tmp[:,i] = random.multivariate_normal(x_0, P_0)
-x_a_enkf[:,0]   = mean(x_a_enkf_tmp,1) # initial state
-P_a_enkf[:,:,0] = cov(x_a_enkf_tmp)    # initial state covariance
+    x_a_enkf_tmp[:,i] = np.random.multivariate_normal(x_0, P_0)
+
+x_a_enkf[:,0]   = np.mean(x_a_enkf_tmp,1) # initial state
+P_a_enkf[:,:,0] = np.cov(x_a_enkf_tmp)    # initial state covariance
+
 for k in range(1,nb): # forward in time
     # prediction step
-    for i in range(Ne):
-        x_f_enkf_tmp[:,i] = m(x_a_enkf_tmp[:,i]) + random.multivariate_normal(zeros(n), Q) ### A CACHER
-        y_f_enkf_tmp[:,i] = H @ x_f_enkf_tmp[:,i] + random.multivariate_normal(zeros(p), R) ### A CACHER
-    P_f_enkf_tmp = cov(x_f_enkf_tmp) ### A CACHER
+    for i in range(2,Ne):
+        x_f_enkf_tmp[:,i] = m(x_a_enkf_tmp[:,i-2:i])[:,1] + np.random.multivariate_normal(np.zeros(n), Q) ### A CACHER
+        y_f_enkf_tmp[:,i] = H @ x_f_enkf_tmp[:,i] + np.random.multivariate_normal(np.zeros(p), R) ### A CACHER
+    P_f_enkf_tmp = np.cov(x_f_enkf_tmp) ### A CACHER
     # Kalman gain
-    K = P_f_enkf_tmp @ H.T @ inv(H @ P_f_enkf_tmp @ H.T + R) ### A CACHER
+    K = P_f_enkf_tmp @ H.T @ np.inv(H @ P_f_enkf_tmp @ H.T + R) ### A CACHER
     # update step
     for i in range(Ne):
         x_a_enkf_tmp[:,i] = x_f_enkf_tmp[:,i] + K @ (y[:,k] - y_f_enkf_tmp[:,i]) ### A CACHER
-    P_a_enkf_tmp = cov(x_a_enkf_tmp) ### A CACHER
+    P_a_enkf_tmp = np.cov(x_a_enkf_tmp) ### A CACHER
     # store results
     x_f_enkf[:,k]   = mean(x_f_enkf_tmp,1)
     P_f_enkf[:,:,k] = P_f_enkf_tmp
